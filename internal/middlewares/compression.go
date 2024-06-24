85b032a7aa0c26c88053d8b5ed240935e25ff732
Igor Morozov <24278723+morozoffnor@users.noreply.github.com>
2024-06-24 18:31:36 +0300

    body gzip fix

--- internal/middlewares/compression.go
+++ internal/middlewares/compression.go
@@ -2,7 +2,6 @@ package middlewares
 
 import (
 	"github.com/morozoffnor/go-url-shortener/internal/types"
-	"log"
 	"net/http"
 	"strings"
 )
@@ -16,17 +15,17 @@ func Compress(h http.HandlerFunc) http.HandlerFunc {
 			defer gzipWriter.Close()
 		}
 
-		if strings.Contains(r.Header.Get("Content-Encoding"), "gzip") {
-			gzipReader, err := types.NewGzipReader(r.Body)
-			if err != nil {
-				log.Print(err.Error())
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-
-			r.Body = gzipReader
-			defer gzipReader.Close()
-		}
+		//if strings.Contains(r.Header.Get("Content-Encoding"), "gzip") {
+		//	gzipReader, err := types.NewGzipReader(r.Body)
+		//	if err != nil {
+		//		log.Print(err.Error())
+		//		w.WriteHeader(http.StatusInternalServerError)
+		//		return
+		//	}
+		//
+		//	r.Body = gzipReader
+		//	defer gzipReader.Close()
+		//}
 
 		h.ServeHTTP(nw, r)
 
